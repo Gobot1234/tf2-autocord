1323a03fc8f039a5bf3375a4146292ed70c0960e
James <gobot1234yt@gmail.com>
2020-08-09 18:44:43 +0100

    Final fixes?

--- setup.py
+++ setup.py
@@ -2,17 +2,9 @@
 
 import setuptools
 
-from autocord import __version__
-
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="tf2-autocord",
-    url="https://github.com/Gobot1234/tf2-autocord",
     requirements=requirements,
-    version=__version__,
-    packages=["autocord", "autocord/cogs", "autocord/cogs/utils"],
-    include_package_data=True,
-    python_requires=">=3.7",
 )
